ed7689521f6f8e242316db47e4d794581b8beba1
Frank Buckley <frank@dsegroup.org>
2022-12-26 16:54:05 +0000

    Get it working!

--- DllMain.cpp
+++ DllMain.cpp
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <strsafe.h>
 #include "include/MinHook.h"
-#include <gl/GL.h>
+#include <glad/include/glad/gl.h>
 #include <stdbool.h>
 //#include <gl/GL.h>
 //#include <gl/GLU.h>
@@ -11,6 +11,36 @@
 #include <imgui/imgui.h>
 #include <imgui/imgui_impl_glfw.h>
 #include <imgui/imgui_impl_opengl3.h>
+
+#define IMGUI_IMPL_OPENGL_LOADER_GLAD2
+
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+#include <GLES2/gl2.h>
+// About Desktop OpenGL function loaders:
+//  Modern desktop OpenGL doesn't have a standard portable header file to load OpenGL function pointers.
+//  Helper libraries are often used for this purpose! Here we are supporting a few common ones (gl3w, glew, glad).
+//  You may use another loader/header of your choice (glext, glLoadGen, etc.), or chose to manually implement your own.
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GL3W)
+#include <GL/gl3w.h>            // Initialize with gl3wInit()
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLEW)
+#include <GL/glew.h>            // Initialize with glewInit()
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLAD)
+#include <glad/glad.h>          // Initialize with gladLoadGL()
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLAD2)
+#include <../include/glad/include/glad/gl.h>		// Initialize with gladLoadGL(...) or gladLoaderLoadGL()
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLBINDING2)
+#define GLFW_INCLUDE_NONE       // GLFW including OpenGL headers causes ambiguity or multiple definition errors.
+#include <glbinding/Binding.h>  // Initialize with glbinding::Binding::initialize()
+#include <glbinding/gl/gl.h>
+using namespace gl;
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLBINDING3)
+#define GLFW_INCLUDE_NONE       // GLFW including OpenGL headers causes ambiguity or multiple definition errors.
+#include <glbinding/glbinding.h>// Initialize with glbinding::initialize()
+#include <glbinding/gl/gl.h>
+using namespace gl;
+#else
+#include IMGUI_IMPL_OPENGL_LOADER_CUSTOM
+#endif
 
 FARPROC wglSwapBuffers = NULL;
 HMODULE m_opengl_dll;
@@ -23,6 +53,7 @@ typedef int (WINAPI* PFNWGLSWAPBUFFERS)(HDC);
 PFNWGLSWAPBUFFERS pfnOrigWglSwapBuffers;
 
 static GLFWwindow* window;
+static ImGuiIO io;
 static ImGuiContext* imgui_context;
 const char* glsl_version = "#version 130";
 
@@ -31,45 +62,63 @@ static void glfw_error_callback(int error, const char* description)
     fprintf(stderr, "Glfw Error %d: %s\n", error, description);
 }
 
-bool init_window()
+DWORD WINAPI init_context()
 {
-
     // Setup window
     glfwSetErrorCallback(glfw_error_callback);
+
     if (!glfwInit())
-        return 1;
+        return FALSE;
 
+    const char* glsl_version = "#version 130";
 
-    // GL 3.0 + GLSL 130
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 
-    // Create window with graphics context
-    window = glfwCreateWindow(1280, 720, "Dear ImGui GLFW+OpenGL3 example", NULL, NULL);
-    if (window == NULL)
-        return 1;
-    glfwMakeContextCurrent(window);
-    glfwSwapInterval(1); // Enable vsync
+    // HWND activeWindowHandle = GetActiveWindow();
 
-    return true;
-}
+    GLFWwindow* currentContext = glfwGetCurrentContext();
 
-bool init_imgui()
-{
-    // Setup Dear ImGui context
+    if (!currentContext)
+    {
+        OutputDebugString(L"No current (GLFW) context.\n");
+        return FALSE;
+    }
+
+    window = currentContext;
+
+    //// Create window with graphics context
+    // window = glfwCreateWindow(800, 600, "Hello World :D", NULL, currentContext);
+    //
+    //if (window == NULL)
+    //{
+    //	return FALSE;
+    //}
+    //
+    //glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+
+    // Initialize OpenGL loader
+
+    bool err = gladLoadGL(glfwGetProcAddress) == 0;
+
+    if (err)
+    {
+        OutputDebugString(L"Failed to initialize OpenGL loader.\n");
+        return FALSE;
+    }
+
     IMGUI_CHECKVERSION();
-    imgui_context = ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    ImGui::CreateContext();
+    io = ImGui::GetIO(); (void)io;
 
-    // Setup Dear ImGui style
     ImGui::StyleColorsDark();
-    //ImGui::StyleColorsLight();
 
-    // Setup Platform/Renderer backends
+    // Setup Platform/Renderer bindings
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init(glsl_version);
 
-    return true;
+    return TRUE;
 }
 
 long wgl_swap_buffers(_In_ HDC hdc) {
@@ -77,79 +126,38 @@ long wgl_swap_buffers(_In_ HDC hdc) {
 
     OutputDebugString(L"wglSwapHook Called\n");
 
-
-    if (window == nullptr)
-    {
-        init_window();
-    }
-
-    if (glfwWindowShouldClose(window))
-    {
-        return 0;
-    }
-
-    if (imgui_context == nullptr)
-    {
-        init_imgui();
-    }
-
-
     // Poll and handle events (inputs, window resize, etc.)
     // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
     // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application, or clear/overwrite your copy of the mouse data.
     // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application, or clear/overwrite your copy of the keyboard data.
+  
     // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
-    glfwPollEvents();
-
-    // Start the Dear ImGui frame
-    ImGui_ImplOpenGL3_NewFrame();
-    ImGui_ImplGlfw_NewFrame();
-    ImGui::NewFrame();
-
-    bool show = true;
-
-    ImGui::ShowDemoWindow(&show);
-
-    /*
-    gameContext = wglGetCurrentContext();
-
-    if (contextCreated == 0)
+    if (window)
     {
+        glfwPollEvents();
 
-        //Create new contextpMC
-        myContext = wglCreateContext(hdc);
+        // Start the Dear ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
 
-        //Make thread use our context
-        wglMakeCurrent(hdc, myContext);
+        bool show = true;
 
+        ImGui::ShowDemoWindow(&show);
 
-        //Setup our context
-        glMatrixMode(GL_PROJECTION);
-        glLoadIdentity();
-        glOrtho(0.0, 640, 480, 0.0, 1.0, -1.0);
-        glMatrixMode(GL_MODELVIEW);
-        glLoadIdentity();
-        glClearColor(0, 0, 0, 1.0);
+        // Rendering
+        ImGui::Render();
+
+        OutputDebugString(L"[Controller] Called ImGui::Render.\n");
+
+        int display_w, display_h;
+
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        //glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+        //glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
     }
-
-    //Make thread use our context
-    wglMakeCurrent(hdc, myContext);
-
-    //Draw
-    glColor3f(0, 1.0f, 0);
-    glBegin(GL_QUADS);
-	
-	//shape
-    glVertex2f(300, 500.0f);
-    glVertex2f(100.0f, 500.0f);
-    glVertex2f(100.0f, 290.0f);
-    glVertex2f(300, 290.0f);
-	
-	
-    glEnd();
-    //Make thread to use games context again
-    wglMakeCurrent(hdc, gameContext);
-*/
     return pfnOrigWglSwapBuffers(hdc);
 
 }
@@ -162,15 +170,16 @@ void hooking(FARPROC wglSwapBuffers, HMODULE m_opengl_dll)
 	MH_CreateHook((LPVOID)wglSwapBuffers, wgl_swap_buffers, (LPVOID*)&pfnOrigWglSwapBuffers); 
     OutputDebugString(L"Created Hook\n");
 
+    if (!init_context())
+    {
+        OutputDebugString(L"Failed to initialize context - no point in hooking...\n");
+        return;
+    }
+
     MH_EnableHook(wglSwapBuffers);
     OutputDebugString(L"Enabled Hook\n");
 
-    while (!unload) {
-        if (GetAsyncKeyState(VK_DELETE)) {
-            unload = 1;
-        }
-        Sleep(250);
-    }
+
 }
 
 void initialise(void)
@@ -199,9 +208,6 @@ void initialise(void)
 
     hooking(wglSwapBuffers, m_opengl_dll);
 
-    while (!unload) {
-        Sleep(500);
-    }
 }
 
 BOOL WINAPI DllMain(
@@ -214,8 +220,8 @@ BOOL WINAPI DllMain(
     {
     case DLL_PROCESS_ATTACH:
     {
-        CloseHandle(CreateThread(0, 0, (LPTHREAD_START_ROUTINE)initialise, 0, 0, 0));
-
+        // CloseHandle(CreateThread(0, 0, (LPTHREAD_START_ROUTINE)initialise, 0, 0, 0));
+        initialise();
         break;
     }
     default: {
